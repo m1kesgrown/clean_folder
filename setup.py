7aeb57bbd60a4e5d602f9ac163349ce5cfbf29ff
m1kesgrown <czygrin.oleksii@gmail.com>
2023-05-29 20:40:57 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='Clean folder function',
+    version='0.0.4',
+    description='This is my frist package from homework',
+    author='Oleksii Chygrin',
+    author_email='czygrin.oleksii@gmail.com',
+    url='https://github.com/m1kesgrown/basics/clean_folder',
+    license='MIT',
+    packages=find_namespace_packages(where='clean_folder'),
+    packages_dir={'': 'src'},
+    classifiers=['Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent'],
+    include_package_data=True,
+    install_requires=[''],
+    entry_points={'console_scripts': ['clean-folder=clean_folder.clean:sort']}
+)
